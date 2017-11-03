ac7eaad2d8726e8f5be4888551a3217d59cbd7dd
pemmons <pemmons@csumb.edu>
2017-11-03 22:52:59 +0000

    yup

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
 app.use( express.logger() );
 
 app.get('/', function( request, response ){
