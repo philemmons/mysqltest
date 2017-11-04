cadee6f38dd4894d3c644b519fde77811183b02a
pemmons <pemmons@csumb.edu>
2017-11-04 23:26:49 +0000

    debug keys

--- app.js
+++ app.js
@@ -5,18 +5,14 @@ var app = express();
 var mysql = require('mysql');
 app.use( express.logger() );
 
-let keys = {
+
+
+
+var connection = mysql.createConnection({
     host: process.env.hostKey,
     user: process.env.userKey,
     password: process.env.pwKey,
     database: process.env.dbKey
-}
-
-var connection = mysql.createConnection({
-  'host'     : keys.host,
-  'user'     : keys.user,
-  'password' : keys.password,
-  'database' : keys.database
 });
  
 connection.connect();
