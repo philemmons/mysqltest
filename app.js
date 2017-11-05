a211eb3ec7315b869445555a11ddf5efb81e2212
pemmons <pemmons@csumb.edu>
2017-11-05 07:35:36 +0000

    minor formating

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
 
-    
-
-
-
-var express = require('express');
-var app = express();
-var mysql = require('mysql');
 app.use( express.logger() );
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host     : process.env.hKey,
   user     : process.env.uKey,
   password : process.env.pwKey,
@@ -17,17 +13,14 @@ var connection = mysql.createConnection({
  
 connection.connect();
 
-
-
 app.get('/', function( request, response ){
     connection.query('SELECT * FROM admin', function (error, results, fields) {
         if (error){
             console.log('Error: ', error);
             throw error;
         }
-       response.send(['hello world  ', results]);
+       response.send(['hello Millie!!!!', results]);
     });
- 
 });
 
 var port = process.env.PORT || 5000;
