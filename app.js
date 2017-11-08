85d667ae6dd8eb3f69d5ec4fe42ffbd71e6881ec
pemmons <pemmons@csumb.edu>
2017-11-08 04:52:33 +0000

    remove socketio, async, server.js and client.html code

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 //https://www.youtube.com/watch?v=2OGHdii_42
 
+//https://www.npmjs.com/package/express-mysql-session
+
+//https://codeburst.io/authenticating-node-js-using-php-sessions-cd1e15b21b53
+
 const express = require('express');
 const app = module.exports = express();
 const mysql = require('mysql');
@@ -23,7 +27,7 @@ app.get('/', function( request, response ){
             console.log('Error: ', error);
             throw error;
         }
-       response.send(['hello!!!!', results]);
+       response.send(['hello Millie!!!!', results]);
     });
 });
 
