844d40cf2557f66d65d579d734f7d7b7670354dd
pemmons <pemmons@csumb.edu>
2017-11-08 05:59:52 +0000

    convert to JAWSdb

--- app.js
+++ app.js
@@ -12,14 +12,26 @@ const mysql = require('mysql');
 
 app.use( express.logger() );
 
-const connection = mysql.createConnection({
-  host     : process.env.hKey,
-  user     : process.env.uKey,
-  password : process.env.pwKey,
-  database : process.env.dbKey
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "db_name",
+    });
+};
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
 });
- 
-connection.connect();
 
 app.get('/', function( request, response ){
     connection.query('SELECT * FROM admin', function (error, results, fields) {
@@ -27,11 +39,33 @@ app.get('/', function( request, response ){
             console.log('Error: ', error);
             throw error;
         }
-       response.send(['hello Millie!!!!', results]);
+      response.send(['hello Millie!!!!', results]);
     });
 });
 
-var port = process.env.PORT || 5000;
+
+//  clearDB
+                
+// const connection = mysql.createConnection({
+//   host     : process.env.hKey,
+//   user     : process.env.uKey,
+//   password : process.env.pwKey,
+//   database : process.env.dbKey
+// });
+ 
+// connection.connect();
+
+// app.get('/', function( request, response ){
+//     connection.query('SELECT * FROM admin', function (error, results, fields) {
+//         if (error){
+//             console.log('Error: ', error);
+//             throw error;
+//         }
+//       response.send(['hello Millie!!!!', results]);
+//     });
+// });
+
+var port = process.env.PORT || 3000;
 app.listen(port, function(){
     console.log("creeping on "+ port);
 });
